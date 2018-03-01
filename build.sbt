d2e6bc1a40401013832211ce0c076164098b97a5
poc <ando934@gmail.com>
2018-03-01 17:47:56 +0100

    Add props files

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "0.1"
 scalaVersion := "2.11.8"
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.1"
+
+libraryDependencies += "com.typesafe" % "config" % "1.3.0"
--- src/main/scala/SimpleScalaSpark.scala
+++ src/main/scala/SimpleScalaSpark.scala
@@ -1,23 +1,32 @@
 /****
   *
   *  Poc project - main class
+  *  spark-submit --class SimpleScalaSpark $HOME/spark/lib/simplescalaspark_2.11-0.1.jar \
+  *  prd hdfs:///user/tandrian/ingestion/input/shakespeare.txt /tmp/shakespeareWordCount1
   */
 package main.scala
 
+import com.typesafe.config.ConfigFactory
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Main {
 
   def main(args: Array[String]) {
-
+    // get args
+    val executionEnvironment = args(0)
+    val inputPath = args(1)
+    val outputPath = args(2)
+    // init props
+    val props = ConfigFactory.load()
     //Create a SparkContext to initialize Spark
     val conf = new SparkConf()
-    //conf.setMaster("local")
+    conf.setMaster(props.getConfig(executionEnvironment).getString("executionMode"))
     conf.setAppName("Word Count")
     val sc = new SparkContext(conf)
 
     // Load the text into a Spark RDD, which is a distributed representation of each line of text
-    val textFile = sc.textFile("hdfs:///user/tandrian/ingestion/input/shakespeare.txt")
+    //val textFile = sc.textFile("hdfs:///user/tandrian/ingestion/input/shakespeare.txt")
+    val textFile = sc.textFile(inputPath)
 
     //word count
     val counts = textFile.flatMap(line => line.split(" "))
@@ -26,7 +35,8 @@ object Main {
 
     counts.foreach(println)
     System.out.println("Total words: " + counts.count());
-    counts.saveAsTextFile("/tmp/shakespeareWordCount1");
+    //counts.saveAsTextFile("/tmp/shakespeareWordCount1");
+    counts.saveAsTextFile(outputPath);
   }
 
 }
