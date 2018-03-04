71ce9e881786c6e1a22947a118f7905a30d573ba
poc <ando934@gmail.com>
2018-03-04 18:53:33 +0100

    class Kafka to hdfs parquet

--- src/main/scala/spark/streaming/CsvToKafka.scala
+++ src/main/scala/spark/streaming/CsvToKafka.scala
@@ -1,15 +1,23 @@
 package main.scala.spark.streaming
 
+/*
+ *  Producer Scala Spark
+ *   /usr/hdp/2.6.3.0-235/spark2/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.2.0 --master yarn  --deploy-mode client --driver-memory 512m  --executor-memory 1g --executor-cores 1  --num-executors 1 --class main.scala.spark.streaming.KafkaStreamingTest simplescalaspark_2.11-0.1.jar
+ *
+ * Consumer shell
+ *  kafka-console-consumer.sh --bootstrap-server 0.0.0.0:6667 --topic test
+ */
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 
 
-object KafkaStreamingTest {
+object CsvToKafka {
   def main(args: Array[String]): Unit = {
     // Create spark session
     val spark = SparkSession
       .builder
-      .appName("Spark-Kafka-Integration")
+      .appName("CSV to Kafka")
       .getOrCreate()
 
     // Create schema
---
+++ src/main/scala/spark/streaming/KafkaToHdfs.scala
@@ -0,0 +1,48 @@
+package main.scala.spark.streaming
+
+/*
+ *  Producer Scala Spark
+ *   /usr/hdp/2.6.3.0-235/spark2/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.2.0 --master yarn  --deploy-mode client --driver-memory 512m  --executor-memory 1g --executor-cores 1  --num-executors 1 --class main.scala.spark.streaming.KafkaStreamingTest simplescalaspark_2.11-0.1.jar
+ *
+ * Consumer shell
+ *  kafka-console-consumer.sh --bootstrap-server 0.0.0.0:6667 --topic test
+ */
+
+import org.apache.spark.sql.SparkSession
+
+
+object KafkaToHdfs {
+  def main(args: Array[String]): Unit = {
+    // Create spark session
+    val spark = SparkSession
+      .builder
+      .appName("Kafka to HDFS")
+      .getOrCreate()
+
+    // Suscribe stream from Kafka
+
+    val df = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "192.168.33.204:9092")
+      .option("subscribe", "test")
+      .load()
+
+
+    // Print
+    df.writeStream
+      .format("console")
+      .option("truncate","false")
+      .start()
+
+    val query = df
+      .writeStream
+      .outputMode("append")
+      .format("parquet")
+      .option("path", "/user/tandrian/parquet")
+      .option("checkpointLocation", "checkpoint")
+      .start()
+
+    query.awaitTermination()
+  }
+}
