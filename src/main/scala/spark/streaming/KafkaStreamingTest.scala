e1dc9f872a2751475a140212d5a1a6a47fcbdf41
poc <ando934@gmail.com>
2018-03-04 15:08:55 +0100

    New test KafkaStreamingTest

--- src/main/scala/spark/streaming/KafkaStreamingTest.scala
+++ src/main/scala/spark/streaming/KafkaStreamingTest.scala
@@ -10,31 +10,31 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object KafkaStreamingTest {
   def main(args: Array[String]): Unit = {
-  val sc = SparkContext.getOrCreate
-  val ssc = new StreamingContext(sc, Seconds(5))
-  val kafkaParams = Map[String, Object](
-    "bootstrap.servers" -> "localhost:9092",
-    "key.deserializer" -> classOf[StringDeserializer],
-    "value.deserializer" -> classOf[StringDeserializer],
-    "group.id" -> "use_a_separate_group_id_for_each_stream",
-    "auto.offset.reset" -> "latest",
-    "enable.auto.commit" -> (false: java.lang.Boolean)
-  )
+    val sc = SparkContext.getOrCreate
+    val ssc = new StreamingContext(sc, Seconds(5))
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "localhost:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "use_a_separate_group_id_for_each_stream",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
 
-  val topics = Array("test")
-  val stream = KafkaUtils.createDirectStream[String, String](
-    ssc,
-    PreferConsistent,
-    Subscribe[String, String](topics, kafkaParams)
-  )
+    val topics = Array("test")
+    val stream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
 
-  print(stream.map(record => (record.key, record.value)))
+    print(stream.map(record => (record.key, record.value)))
 
-  ssc.start
+    ssc.start
 
-  // the above code is printing out topic details every 5 seconds
-  // until you stop it.
+    // the above code is printing out topic details every 5 seconds
+    // until you stop it.
 
-  ssc.stop(stopSparkContext = false)
-}
+    ssc.stop(stopSparkContext = false)
+  }
 }
