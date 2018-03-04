8c4bb66f58a95cbaaa17e7f2b83040bbdb13ad53
poc <ando934@gmail.com>
2018-03-04 18:19:03 +0100

    New test KafkaStreamingTest

--- src/main/scala/spark/streaming/KafkaStreamingTest.scala
+++ src/main/scala/spark/streaming/KafkaStreamingTest.scala
@@ -23,6 +23,13 @@ object KafkaStreamingTest {
 
     //Create the Streaming DataFrame
     val streamingDataFrame = spark.readStream.schema(mySchema).csv("/user/tandrian/ingestion/streaming/")
+
+    streamingDataFrame.writeStream
+      .format("console")
+      .option("truncate","false")
+      .start()
+
+    val df = streamingDataFrame
       .selectExpr("CAST(id AS STRING) AS key", "to_json(struct(*)) AS value")
       .writeStream
       .format("kafka")
@@ -31,40 +38,40 @@ object KafkaStreamingTest {
       .option("topic", "test")
       .start()
 
-    streamingDataFrame.awaitTermination()
+    df.awaitTermination()
 
-    /*// Publish stream to kafka
-    streamingDataFrame.selectExpr("CAST(id AS STRING) AS key", "to_json(struct(*)) AS value")
-      .writeStream
-      .format("kafka")
-      .option("topic", "test")
-      .option("kafka.bootstrap.servers", "192.168.33.204:9092")
-      .option("checkpointLocation", "/home/tandrian/spark/checkpoint")*/
+/*// Publish stream to kafka
+streamingDataFrame.selectExpr("CAST(id AS STRING) AS key", "to_json(struct(*)) AS value")
+.writeStream
+.format("kafka")
+.option("topic", "test")
+.option("kafka.bootstrap.servers", "192.168.33.204:9092")
+.option("checkpointLocation", "/home/tandrian/spark/checkpoint")*/
 
-    // Suscribe stream from Kafka
+// Suscribe stream from Kafka
 
-    /*val df = spark
-      .readStream
-      .format("kafka")
-      .option("kafka.bootstrap.servers", "192.168.33.204:9092")
-      .option("subscribe", "test")
-      .load()*/
+/*val df = spark
+.readStream
+.format("kafka")
+.option("kafka.bootstrap.servers", "192.168.33.204:9092")
+.option("subscribe", "test")
+.load()*/
 
 
-    // Print
-    /*df.writeStream
-      .format("console")
-      .option("truncate","false")
-      .start()
-      .awaitTermination()*/
+// Print
+/*df.writeStream
+.format("console")
+.option("truncate","false")
+.start()
+.awaitTermination()*/
 
-    /*val query = df
-      .writeStream
-      .outputMode("append")
-      .format("parquet")
-      .option("path", "/user/tandrian/parquet")
-      .option("checkpointLocation", "checkpoint")
-      .start()
-      .awaitTermination()*/
-  }
+/*val query = df
+.writeStream
+.outputMode("append")
+.format("parquet")
+.option("path", "/user/tandrian/parquet")
+.option("checkpointLocation", "checkpoint")
+.start()
+.awaitTermination()*/
+}
 }
