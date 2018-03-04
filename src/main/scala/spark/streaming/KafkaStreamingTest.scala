20d6664baffbca237e291ce5f6f3f28d9d6f2c34
poc <ando934@gmail.com>
2018-03-04 15:14:30 +0100

    New test KafkaStreamingTest

--- src/main/scala/spark/streaming/KafkaStreamingTest.scala
+++ src/main/scala/spark/streaming/KafkaStreamingTest.scala
@@ -1,16 +1,9 @@
 package main.scala.spark.streaming
 
-import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.spark.SparkContext
-import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
-import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
-import org.apache.spark.streaming.kafka010._
-import org.apache.spark.streaming.{Seconds, StreamingContext}
-
 
 object KafkaStreamingTest {
   def main(args: Array[String]): Unit = {
-    val sc = SparkContext.getOrCreate
+   /* val sc = SparkContext.getOrCreate
     val ssc = new StreamingContext(sc, Seconds(5))
     val kafkaParams = Map[String, Object](
       "bootstrap.servers" -> "localhost:9092",
@@ -35,6 +28,6 @@ object KafkaStreamingTest {
     // the above code is printing out topic details every 5 seconds
     // until you stop it.
 
-    ssc.stop(stopSparkContext = false)
+    ssc.stop(stopSparkContext = false) */
   }
 }
